410f061992ea84ad9b21b99737c1fb07129e510f
mehedi hasan khairul <mhkhairul78@gmail.com>
2022-06-04 11:48:46 +0600

    layout done

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
+
+const { Title } = Typography;
+
+const Homepage = () => {
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h Volume" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market" value="5" />
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Homepage;
